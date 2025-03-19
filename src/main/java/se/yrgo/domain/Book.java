4d9855a4dfa1513b643ced99dc345e0cba540670
André <andre.ljungsvik@gmail.com>
2025-03-19 19:22:35 +0100

    Book class

--- src/main/java/se/yrgo/domain/Book.java
+++ src/main/java/se/yrgo/domain/Book.java
@@ -1,4 +1,52 @@
 package se.yrgo.domain;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String title;
+    private String author;
+    @ManyToOne
+    @JoinColumn(name = "library_id")
+    private Library library;
+
+    public Book() {
+    }
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public void setLibrary(Library library) {
+        this.library = library;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Library getLibrary() {
+        return library;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
 }
